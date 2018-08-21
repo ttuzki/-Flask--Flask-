eaddf0c78eca91a8929238f918ebb47f14f6947d
ttuzki <916548019@qq.com>
2018-08-21 17:31:16 +0800

    完成课时43-登陆页面完成

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 import config
 
 app = Flask(__name__)
@@ -8,9 +8,12 @@ app.config.from_object(config)
 def index():
 	return render_template('index.html')
 
-@app.route('/login/')
+@app.route('/login/',methods=['GET','POST'])
 def login():
-	return render_template('login.html')
+	if request.method == 'GET':
+		return render_template('login.html')
+	else:
+		pass
 
 if __name__ == '__main__':
 	app.run()
