192cb4392522c63f40ba3fa50de33416b209c101
ttuzki <916548019@qq.com>
2018-08-21 10:43:06 +0800

    完成40课时，完成基本的环境搭建

---
+++ manage.py
@@ -0,0 +1,13 @@
+from flask_script import Manager
+from flask_migrate import Migrate,MigrateCommand
+from run import app
+from exts import db
+
+manager = Manager(app)
+
+migrate = Migrate(app,db)
+
+manager.add_command('db',MigrateCommand)
+
+if __name__ == '__main__':
+	manager.run()
