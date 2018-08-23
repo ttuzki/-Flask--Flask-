9f12039a89f9d564ec5227e9f3dfb0921b8b4411
ttuzki <916548019@qq.com>
2018-08-23 17:17:10 +0800

    完成课时46-注册页面完成

--- config.py
+++ config.py
@@ -10,3 +10,4 @@ DATABASE = 'zlkt'
  
 SQLALCHEMY_DATABASE_URI = "{}+{}://{}:{}@{}:{}/{}?charset=utf8".format(DIALECT,DRIVER,USERNAME,PASSWORD,HOST
                                              ,PORT,DATABASE)
+SQLALCHEMY_TRACK_MODIFICATIONS = False
--- run.py
+++ run.py
@@ -1,8 +1,11 @@
-from flask import Flask,render_template,request
+from flask import Flask,render_template,request,redirect,url_for
 import config
+from models import User
+from exts import db
 
 app = Flask(__name__)
 app.config.from_object(config)
+db.init_app(app)
 
 @app.route('/')
 def index():
@@ -20,7 +23,24 @@ def regist():
 	if request.method == 'GET':
 		return render_template('regist.html')
 	else:
-		pass
+		telephone = request.form.get('telephone')
+		username = request.form.get('username')
+		password1 = request.form.get('password1')
+		password2 = request.form.get('password2')
+
+		user = User.query.filter(User.telephone == telephone).first()
+		if user:
+			return '该手机号码已被注册，请更换手机号码！'
+		else:
+			if password1 != password2:
+				return '两次密码不相等，请核对后再填写！'
+			else:
+				user = User(telephone = telephone,username = username,password = password1)
+				db.session.add(user)
+				db.session.commit()
+
+				return redirect(url_for('login'))
+		
 
 if __name__ == '__main__':
 	app.run()
